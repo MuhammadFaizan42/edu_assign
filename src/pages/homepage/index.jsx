f4bed367c9948c89508e0a89846aa14ec3d75abd
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-05-07 21:25:43 +0500

    assign start

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from './features/banner'
+import Skills from './features/skills'
+import Tracks from './features/tracks'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Banner />
+            <Skills />
+            <Tracks />
+        </div>
+    )
+}
+
+export default HomePage
