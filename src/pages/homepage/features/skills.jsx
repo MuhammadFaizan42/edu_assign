81d2720b383e90c6152287f77a3079f7d3da2532
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-05-09 00:13:50 +0500

    banner update

--- src/pages/homepage/features/skills.jsx
+++ src/pages/homepage/features/skills.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import Icon from '../../../assets/computer.png'
+import Vector from '../../../assets/Vector.png'
+import Codecov from '../../../assets/Codecov (logo — Black).png'
+import userTesting from '../../../assets/UserTesting (logo — Black).png'
+import Magic from '../../../assets/Magic Leap (logo — Black).png'
+
 
 const Skills = () => {
     return (
@@ -8,30 +12,32 @@ const Skills = () => {
             <div className="page_width">
                 <div className="skills_container">
                     <div className="item_grid">
-                        <div className="img">
-                            <img src={Icon} alt="" />
+                        <div className="">
                         </div>
                         <div className="content">
-                            <h3>Learn The Latest Skills</h3>
-                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old.</p>
+                            <h6>250+</h6>
+                            <p>Collaboration</p>
                         </div>
                     </div>
                     <div className="item_grid">
                         <div className="img">
-                            <img src={Icon} alt="" />
+                            <img src={Vector} alt="" />
                         </div>
-                        <div className="content">
-                            <h3>Learn The Latest Skills</h3>
-                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old.</p>
+                        
+                    </div>
+                    <div className="item_grid">
+                        <div className="img">
+                            <img src={Codecov} alt="" />
                         </div>
                     </div>
                     <div className="item_grid">
                         <div className="img">
-                            <img src={Icon} alt="" />
+                            <img src={userTesting} alt="" />
                         </div>
-                        <div className="content">
-                            <h3>Learn The Latest Skills</h3>
-                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old.</p>
+                    </div>
+                    <div className="item_grid">
+                        <div className="img">
+                            <img src={Magic} alt="" />
                         </div>
                     </div>
 
