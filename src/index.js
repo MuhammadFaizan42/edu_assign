e98ab6b3dd22762f21e884bae53c0591bcea2597
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-05-31 00:54:02 +0500

    contact page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './styles/designtrack.css'
 import './styles/heroes.css'
 import './styles/student.css'
 import './styles/blog.css'
+import './pages/contactUsPage/style.css'
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
--- src/pages/contactUsPage/index.jsx
+++ src/pages/contactUsPage/index.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const ContactUs = () => {
     return (
-        <div>ContactUs</div>
+        
+    <div className="outer_contact">
+        <div className="page_width">
+            <div className="inner_contact">
+                        <h2>CONTACT US</h2>
+                        <div className="contact_cards">
+                            
+                              <div className="contact_fields">
+                              <p>Leave us a message</p>
+                                <input type="text" className='name_input' placeholder='Your name' label='Name' />
+                                <input type="text" className='email_input' placeholder='Email Address' />
+                                <textarea type="text" className='text_input' placeholder='Your message' />
+                              </div>
+                        </div>
+             </div>
+        </div>
+             
+    </div>
+       
     )
 }
 
